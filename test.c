53ed62ea6a9e40895337e59d9d5d781b43a05e35
lsp <lsp@gitee.com>
2021-03-04 20:11:35 +0800

    first commit of test

--- test.c
+++ test.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
 #include<string.h>
-//#define tokenpaster(n) printf ("token" #n " = %d\n", token##n)
-#pragma pack(2)
-struct node{
-	char r;
-	int a;
-	double d;
-	char c;
-};
+
 int main(void)
 {
-	printf("%llu\n",sizeof(struct node));
+	printf(hello,world!\n");
 	
 	return 0;
 }
